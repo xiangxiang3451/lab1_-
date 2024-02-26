0fe9913b68da4d72252e4e51ca653d95a2157055
Chang <xiangxiang3451@gmail.com>
2024-02-27 01:17:36 +0700

    first commit

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtStart = new System.Windows.Forms.Button();
+            this.EdSpeed = new System.Windows.Forms.NumericUpDown();
+            this.EdAngle = new System.Windows.Forms.NumericUpDown();
+            this.EdHeight = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EdSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EdAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EdHeight)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BtStart);
+            this.panel1.Controls.Add(this.EdSpeed);
+            this.panel1.Controls.Add(this.EdAngle);
+            this.panel1.Controls.Add(this.EdHeight);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1036, 147);
+            this.panel1.TabIndex = 0;
+            // 
+            // BtStart
+            // 
+            this.BtStart.Location = new System.Drawing.Point(405, 65);
+            this.BtStart.Name = "BtStart";
+            this.BtStart.Size = new System.Drawing.Size(119, 42);
+            this.BtStart.TabIndex = 6;
+            this.BtStart.Text = "Start";
+            this.BtStart.UseVisualStyleBackColor = true;
+            this.BtStart.Click += new System.EventHandler(this.BtStart_Click);
+            // 
+            // EdSpeed
+            // 
+            this.EdSpeed.Location = new System.Drawing.Point(247, 112);
+            this.EdSpeed.Name = "EdSpeed";
+            this.EdSpeed.Size = new System.Drawing.Size(120, 28);
+            this.EdSpeed.TabIndex = 5;
+            this.EdSpeed.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // EdAngle
+            // 
+            this.EdAngle.Location = new System.Drawing.Point(247, 67);
+            this.EdAngle.Name = "EdAngle";
+            this.EdAngle.Size = new System.Drawing.Size(120, 28);
+            this.EdAngle.TabIndex = 4;
+            this.EdAngle.Value = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            // 
+            // EdHeight
+            // 
+            this.EdHeight.Location = new System.Drawing.Point(247, 28);
+            this.EdHeight.Name = "EdHeight";
+            this.EdHeight.Size = new System.Drawing.Size(120, 28);
+            this.EdHeight.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Speed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angle";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(-9, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1044, 426);
+            this.panel2.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Maximum = 25D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 6D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderColor = System.Drawing.Color.Red;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1044, 426);
+            this.chart1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 569);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.EdSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EdAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EdHeight)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtStart;
+        private System.Windows.Forms.NumericUpDown EdSpeed;
+        private System.Windows.Forms.NumericUpDown EdAngle;
+        private System.Windows.Forms.NumericUpDown EdHeight;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double dt = 0.01;
+        double height, angle, speed;
+        double t, x, y;
+
+
+        double cosa, sina;
+        const double g = 9.81;
+        int index = -1;
+
+        private void BtStart_Click(object sender, EventArgs e)
+        {
+
+            height = (double)EdHeight.Value;
+            angle = (double)EdAngle.Value;
+            speed = (double)EdSpeed.Value;
+
+            cosa = Math.Cos(angle * Math.PI / 180);
+            sina = Math.Sin(angle * Math.PI / 180);
+
+            t = 0;
+            x = 0;
+            y = height;
+
+
+            chart1.Series[index + 1].Points.Clear();
+            chart1.Series[index + 1].Points.AddXY(x, y);
+
+
+
+            index++;
+            timer1.Start();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            x = speed * cosa * t;
+            y = speed * sina * t + height - g * t * t / 2;
+            chart1.Series[index].Points.AddXY(x, y);
+
+            if (y <= 0) timer1.Stop();
+
+        }
+    }
+
+}
+
